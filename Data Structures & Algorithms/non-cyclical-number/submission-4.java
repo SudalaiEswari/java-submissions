4ce69f3796fd9ed8a45bccb65f43ee3b7544d0ee
Sudalai Eswari S <ssudalaieswari@gmail.com>
2026-07-11 16:14:58 +0530

    Add: non-cyclical-number - submission-4

---
+++ Data Structures & Algorithms/non-cyclical-number/submission-4.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean isHappy(int n) {
+        while(n>9)
+        {
+        int sum =0;
+       
+        
+        while(n !=0)
+        {  
+            int digit = n%10;
+            digit = digit * digit;
+            sum = sum + digit;
+            n = n/10;
+        }
+        n = sum;
+        }
+        if(n==1 || n== 7)
+        {
+            return true;
+        }
+        return false;
+    }
+}
